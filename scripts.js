580f4b9b2540db5cad1dc3aeb37f6f24de9d7fd2
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-11-21 10:52:10 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -27,20 +27,12 @@ function price(){
    }
  }
  function addtocart(){
-//var trStart = document.createElement("tr");
-//var trEnd = document.body.appendChild(trStart);
-//var tdStart = document.createElement("td");
-//var tdEnd = document.body.appendChild(tdStart);
-//document.cookie += trStart + tdStart + document.getElementById('item').innerHTML + tdEnd + tdStart + document.getElementById('amount').value; + tdEnd + tdStart + document.getElementById('price').innerHTML; + tdEnd + trEnd;
 
-//document.cookie += document.createElement("tr"); + document.createElement("td"); + document.getElementById('item').innerHTML; + document.body.appendChild(document.createElement("td")); + document.createElement("td"); + document.getElementById('amount').value; + document.body.appendChild(document.createElement("td")); + document.createElement("td"); + document.getElementById('price').innerHTML; + document.body.appendChild(document.createElement("td")); + document.body.appendChild(document.createElement("tr"));
-//  document.cookie += "<tr> <td>" + document.getElementById('item').innerHTML + "</td> <td>" + document.getElementById('amount').value + "</td> <td>" + document.getElementById('price').innerHTML + "</td> </tr>";
   document.cookie += document.getElementById('item').innerHTML + document.getElementById('amount') + document.getElementById('price').innerHTML;
   document.getElementById('ordertable').innerHTML = document.cookie;
   document.getElementById('test').innerHTML = document.cookie;
 }
 function ordertable(){
-  //  document.getElementById('ordertable').innerHTML = document.cookie;
-//document.getElementById('ordertable').innerHTML = document.createElement("tr") + document.createElement("td") + "document.getElementById('item').innerHTML" + document.body.appendChild(document.createElement("td")) + document.createElement("td") + "document.getElementById('amount').value" + document.body.appendChild(document.createElement("td"))  + document.createElement("td") + "document.getElementById('price').innerHTML" + document.body.appendChild(document.createElement("td"))  + document.body.appendChild(document.createElement("tr"));
+
 document.getElementById('ordertable').innerHTML = document.cookie;
 }
