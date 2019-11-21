d36b955517af232986ab474f77b0a6b93648b352
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-11-21 12:35:54 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -28,10 +28,11 @@ function price(){
  }
  function addtocart(){
 
- document.cookie += "<div>" + document.getElementById('item').innerHTML + "</div>" + "<div>" + document.getElementById('amount').value + "</div>" + "<div>" + document.getElementById('price').innerHTML + "</div>" + "<br>";
+ document.cookie += document.getElementById('item').innerHTML + document.getElementById('amount').value + document.getElementById('price').innerHTML + "<br>";
   document.getElementById('ordertable').innerHTML = document.cookie;
 // document.getElementById('test').innerHTML += document.getElementById('item').innerHTML + document.getElementById('amount').value + document.getElementById('price').innerHTML + "<br>";
 }
 function ordertable(){
+
 document.getElementById('ordertable').innerHTML = document.cookie;
 }
