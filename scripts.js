bfe2e2e742441c527bc2801d5d02bdc9e45d765d
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-11-20 20:38:50 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -29,7 +29,7 @@ function price(){
  function addtocart(){
 
 
-document.cookie += document.createElement("tr"); + document.createElement("td"); + document.getElementById('item').innerHTML; + document.body.appendChild(document.createElement("td")); + document.createElement("td"); document.getElementById('amount').value; document.body.appendChild(document.createElement("td")); document.createElement("td"); document.getElementById('price').innerHTML; document.body.appendChild(document.createElement("td")); document.body.appendChild(document.createElement("tr"));
+document.cookie += document.createElement("tr"); + document.createElement("td"); + document.getElementById('item').innerHTML; + document.body.appendChild(document.createElement("td")); + document.createElement("td"); + document.getElementById('amount').value; + document.body.appendChild(document.createElement("td")); + document.createElement("td"); + document.getElementById('price').innerHTML; + document.body.appendChild(document.createElement("td")); + document.body.appendChild(document.createElement("tr"));
 //  document.cookie += "<tr> <td>" + document.getElementById('item').innerHTML + "</td> <td>" + document.getElementById('amount').value + "</td> <td>" + document.getElementById('price').innerHTML + "</td> </tr>";
 
   document.getElementById('ordertable').innerHTML = document.cookie;
