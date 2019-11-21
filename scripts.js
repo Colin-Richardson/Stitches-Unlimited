19f97b58212c32da6e26e99359188956aabf16a3
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-11-20 20:05:58 -0600

    please work

--- scripts.js
+++ scripts.js
@@ -27,20 +27,19 @@ function price(){
    }
  }
  function addtocart(){
-  var table = document.getElementById("ordercart");
-  var row = table.insertRow(-1);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  cell1.innerHTML = document.getElementById('item').innerHTML;
-  cell2.innerHTML = document.getElementById('amount').value;
-  cell3.innerHTML =  document.getElementById('price').innerHTML;
-}
+
+//  var table = document.getElementById("ordercart");
+//  var cell1 = row.insertCell(0);
+//  var cell2 = row.insertCell(1);
+//  var cell3 = row.insertCell(2);
+//  cell2.innerHTML = document.getElementById('amount').value;
+document.cookie += document.createElement("tr") + document.createElement("td") + document.getElementById('item').innerHTML + document.body.appendChild(document.createElement("td")) + document.createElement("td") + document.getElementById('amount').value + document.body.appendChild(document.createElement("td"))  + document.createElement("td") + document.getElementById('price').innerHTML +document.body.appendChild(document.createElement("td"))  + document.body.appendChild(document.createElement("tr"));
 //  document.cookie += "<tr> <td>" + document.getElementById('item').innerHTML + "</td> <td>" + document.getElementById('amount').value + "</td> <td>" + document.getElementById('price').innerHTML + "</td> </tr>";
 
   document.getElementById('ordertable').innerHTML = document.cookie;
   document.getElementById('test').innerHTML = document.cookie;
 }
 function ordertable(){
-    document.getElementById('ordertable').innerHTML = document.cookie;
+  //  document.getElementById('ordertable').innerHTML = document.cookie;
+
 }
