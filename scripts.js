2bc70987a3d74599b912dad1d5c6d76e578ff7ee
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-11-22 12:15:16 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -50,4 +50,5 @@ function ordertable(){
  //cell2.innerHTML = localStorage.getItem("amount");
  //cell3.innerHTML = localStorage.getItem("price");
 document.getElementById('ordertable') = document.cookie;
+
 }
