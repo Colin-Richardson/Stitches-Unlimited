bfc05316e7b6ca895d69d7cd4a9a46d335967c5e
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-11-23 16:32:23 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,36 +26,33 @@ function price(){
      document.getElementById('price').innerHTML = document.getElementById('price6').innerHTML;
    }
  }
- function addtocart(){
-   //counts number of clicks
-   if(typeof(Storage) !== "undefined") {
-     if (sessionStorage.clickcount) {
-       sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
-     } else {
-       sessionStorage.clickcount = 1;
-     }
-   }
 
-var array = [document.getElementById('item').innerHTML];
-if(document.cookie){
-array.push(document.cookie);
-}
-document.cookie = JSON.stringify(array);
-   // stores item, price and amount
-sessionStorage.setItem("item", JSON.stringify(item));
-sessionStorage.setItem("amount", document.getElementById('amount').value);
-sessionStorage.setItem("price",  document.getElementById('price').innerHTML);
+function addtocart(){
+  //counts number of clicks
+  if(typeof(Storage) !== "undefined") {
+    if (sessionStorage.clickcount) {
+      sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+    } else {
+      sessionStorage.clickcount = 1;
+    }
+  }
+
+  // stores item, price and amount
+//sessionStorage.setItem("item", JSON.stringify(item));
+//sessionStorage.setItem("amount", document.getElementById('amount').value);
+//sessionStorage.setItem("price",  document.getElementById('price').innerHTML);
 document.getElementById('test').innerHTML = document.cookie;
 }
+
 function ordertable(){
-
-var table = document.getElementById("ordertable");
-var row = table.insertRow(-1);
-var cell1 = row.insertCell(0);
-var cell2 = row.insertCell(1);
-var cell3 = row.insertCell(2);
-cell1.innerHTML = JSON.parse(sessionStorage.getItem('item'));
-cell2.innerHTML = sessionStorage.getItem("amount");
-cell3.innerHTML = sessionStorage.getItem("price");
-
+  var row = document.createElement("TR");
+  row.setAttribute("id", "row");
+  document.getElementById("ordertable").appendChild(row);
+  var td = document.createElement("TD");
+  var celltext = document.createTextNode("asdfsdf");
+  td.appendChild(celltext);
+  var td2 = document.createElement("TD");
+  var celltext2 = document.createTextNode("asdfsdf");
+  td.appendChild(celltext2);
+  document.getElementById("row").appendChild(td);
 }
