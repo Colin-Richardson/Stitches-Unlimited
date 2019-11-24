cb5afee14c8a04500dde26acb096faa23621b4f1
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-11-23 20:42:36 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,29 +30,50 @@ function price(){
 function addtocart(){
   //counts number of clicks
   if(typeof(Storage) !== "undefined") {
-    if (sessionStorage.clickcount) {
-      sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
     } else {
-      sessionStorage.clickcount = 1;
+      localStorage.clickcount = 1;
     }
   }
-
+  var itemString = "item";
+  var amountString = "amount";
+  var priceString = "price";
+  var itemKey = localStorage.getItem('clickcount')
+localStorage[itemString + itemKey] =  document.getElementById('item').innerHTML;
+localStorage[amountString + itemKey] =  document.getElementById('amount').value;
+localStorage[priceString + itemKey] =  document.getElementById('price').innerHTML;
   // stores item, price and amount
-//sessionStorage.setItem("item", JSON.stringify(item));
-//sessionStorage.setItem("amount", document.getElementById('amount').value);
-//sessionStorage.setItem("price",  document.getElementById('price').innerHTML);
-document.getElementById('test').innerHTML = document.cookie;
+//localStorage.setItem("item", JSON.stringify(item));
+//localStorage.setItem("amount", document.getElementById('amount').value);
+//localStorage.setItem("price",  document.getElementById('price').innerHTML);
+document.getElementById('test').innerHTML = localStorage.getItem('item70');
 }
 
 function ordertable(){
+  var itemString = "item";
+  var amountString = "amount";
+  var priceString = "price";
+  var itemKey = localStorage.getItem('clickcount');
+
+
+//creates a row
   var row = document.createElement("TR");
   row.setAttribute("id", "row");
   document.getElementById("ordertable").appendChild(row);
+//creates first td and content
   var td = document.createElement("TD");
-  var celltext = document.createTextNode("asdfsdf");
+  var celltext = document.createTextNode(localStorage.getItem(itemString + itemKey));
   td.appendChild(celltext);
-  var td2 = document.createElement("TD");
-  var celltext2 = document.createTextNode("asdfsdf");
-  td.appendChild(celltext2);
   document.getElementById("row").appendChild(td);
+//creates second td and content
+  var td2 = document.createElement("TD");
+  var celltext2 = document.createTextNode(localStorage.getItem(amountString + itemKey));
+  td2.appendChild(celltext2);
+  document.getElementById("row").appendChild(td2);
+//creates third td and content
+  var td3 = document.createElement("TD");
+  var celltext3 = document.createTextNode(localStorage.getItem(priceString + itemKey));
+  td3.appendChild(celltext3);
+  document.getElementById("row").appendChild(td3);
 }
