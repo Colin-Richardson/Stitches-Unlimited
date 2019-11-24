1079c6a1db756a7ffebc2aa1a3642ae4fdd825c0
Matthew Thornton <53791828+Matthew-Thornton@users.noreply.github.com>
2019-11-24 01:56:54 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -28,51 +28,71 @@ function price(){
      document.getElementById('price').innerHTML = document.getElementById('price6').innerHTML * 200;
    }
  }
+
 //stores item data
 function addtocart(){
   //counts number of clicks
   if(typeof(Storage) !== "undefined") {
-    if (localStorage.clickcount) {
-      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    if (sessionStorage.clickcount) {
+      sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
     } else {
-      localStorage.clickcount = 1;
+      sessionStorage.clickcount = 1;
     }
   }
 //creates vars needed for function
   var itemString = "item";
   var amountString = "amount";
   var priceString = "price";
-  var itemKey = localStorage.getItem('clickcount')
+  var itemKey = sessionStorage.getItem('clickcount')
 //generates itemKey with a number for every button press
-localStorage[itemString + itemKey] =  document.getElementById('item').innerHTML;
-localStorage[amountString + itemKey] =  document.getElementById('amount').value;
-localStorage[priceString + itemKey] =  document.getElementById('price').innerHTML;
+sessionStorage[itemString + itemKey] =  document.getElementById('item').innerHTML;
+sessionStorage[amountString + itemKey] =  document.getElementById('amount').value;
+sessionStorage[priceString + itemKey] =  document.getElementById('price').innerHTML;
+
 }
 //creates table based on stored item data
 function ordertable(){
+
 //creates vars needed for function
   var itemString = "item";
   var amountString = "amount";
   var priceString = "price";
-  var itemKey = localStorage.getItem('clickcount');
+  var itemKey = sessionStorage.getItem('clickcount');
 // loop that loops for each recorded button press(clickcount)
 for(i = 0; i < itemKey; i++){
   var ii = i + 1;
+if (sessionStorage.getItem(itemString + ii) !== null){
 //selects table
   var table = document.getElementById("ordertable");
 //inserts a row(starting on row 1)
-  var row = table.insertRow(ii);
+  var row = table.insertRow(-1);
 //creates 3 cells for each row
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
+  var cell4 = row.insertCell(3);
 // gives each cell a class
 cell1.setAttribute("class", "cell1");
 cell2.setAttribute("class", "cell2");
 cell3.setAttribute("class", "cell3");
+cell4.setAttribute("class", "cell4")
 //names each cell
-  cell1.innerHTML = localStorage.getItem(itemString + ii);
-  cell2.innerHTML = localStorage.getItem(amountString + ii);
-  cell3.innerHTML = localStorage.getItem(priceString + ii);
+  cell1.innerHTML = sessionStorage.getItem(itemString + ii);
+  cell2.innerHTML = sessionStorage.getItem(amountString + ii);
+  cell3.innerHTML = sessionStorage.getItem(priceString + ii);
+  cell4.innerHTML = '<input type="button" value="Delete" onclick="deleteRow(this)">';
+  //remove comment below if sessionStoragebreaks
+  //sessionStorage.clear();
 }
+}
+}
+function deleteRow(r) {
+  var d = r.parentNode.parentNode.rowIndex;
+  var itemString = "item";
+  var amountString = "amount";
+  var priceString = "price";
+  sessionStorage.removeItem(itemString + d);
+  sessionStorage.removeItem(amountString + d);
+  sessionStorage.removeItem(priceString + d);
+  document.getElementById("ordertable").deleteRow(d);
 }
